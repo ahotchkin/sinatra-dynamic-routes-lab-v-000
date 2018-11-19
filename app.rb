0fc188751493ec3bd512ceacd330a7b92b3a575b
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-11-19 21:27:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,7 +19,6 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = ""
     if params[:operation] == "add"
       @operation = "+"
     elsif params[:operation] == "subtract"
