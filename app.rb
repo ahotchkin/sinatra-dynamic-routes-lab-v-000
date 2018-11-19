1a19c13f24af6268e27b35f307e7fbff1f025180
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-11-19 21:22:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,15 +21,15 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     @operation = params[:operation]
 
-    if @operation == "add"
-      +
-    elsif @operation == "subtract"
-      -
-    elsif @operation == "multiply"
-      *
-    elsif @operation == "divide"
-      /
-    end
+    # if @operation == "add"
+    #   +
+    # elsif @operation == "subtract"
+    #   -
+    # elsif @operation == "multiply"
+    #   *
+    # elsif @operation == "divide"
+    #   /
+    # end
     "#{params[:number1].to_i} #{@operation} #{params[:number2].to_i}"
   end
 end
